7d49778a460d8e1cdfd45bcdfef64b88bd32695e
Daniel Grützmacher <daniel@gruetzmacher.at>
2018-11-14 14:27:52 +0100

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name = 'redart',
-    version = '0.1.1',
+    version = '0.1.2',
     packages = ['redart'],
     description = 'A Redmine Artifact manager',
     author = 'René Groß',
